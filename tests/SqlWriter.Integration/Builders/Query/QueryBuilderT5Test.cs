2ff1803d8c6098422fa9a05e4b343e6a9740809a
bcovy <lazy249@gmail.com>
2025-02-05 21:59:27 -0800

    Test and pipeline corrections.

--- tests/SqlWriter.Integration/Builders/Query/QueryBuilderT5Test.cs
+++ tests/SqlWriter.Integration/Builders/Query/QueryBuilderT5Test.cs
@@ -4,10 +4,15 @@ namespace SqlWriter.Integration.Builders.Query;
 
 public class QueryBuilderT5Test
 {
+    
     [Fact]
     public void With_should_add_cte_statement_using_join_type_expression()
     {
+#if OSX
         string expected = "WITH cteA AS (SELECT a.PropertyID, a.Address FROM Table1 AS a\n)\nSELECT a.PropertyID, cteA.PropertyID, cteA.Address FROM Table1 AS a\n JOIN Table2 AS b ON a.PropertyID = b.PropertyID LEFT OUTER JOIN Table3 AS c ON b.PropertyID = c.PropertyID JOIN Table4 AS d ON b.PropertyID = d.PropertyID LEFT OUTER JOIN Table5 AS e ON a.PropertyID = e.PropertyID JOIN cteA ON a.Address = cteA.Address WHERE b.EventID = @p0";
+#else
+        string expected = "WITH cteA AS (SELECT a.PropertyID, a.Address FROM Table1 AS a\n)\nSELECT a.PropertyID, cteA.PropertyID, cteA.Address FROM Table1 AS a\n JOIN Table2 AS b ON a.PropertyID = b.PropertyID LEFT OUTER JOIN Table3 AS c ON b.PropertyID = c.PropertyID JOIN Table4 AS d ON b.PropertyID = d.PropertyID LEFT OUTER JOIN Table5 AS e ON a.PropertyID = e.PropertyID JOIN cteA ON a.Address = cteA.Address WHERE b.EventID = @p0";
+#endif
         ICteStatement cte = SqlWriters.QueryAsCte<QueryableMod1>("cteA", includeCteJoinColumn: true).Select(a => new { a.PropertyID, a.Address });
 
         var statement = SqlWriters.Query<QueryableMod1, QueryableMod2, QueryableMod3, QueryableMod4, QueryableMod5>()
